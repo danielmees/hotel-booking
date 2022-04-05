7dec9558df12ce0e3e3810d6a0af61679c470aa1
Bin Mi <danielmees2014@gmail.com>
2022-04-05 21:46:30 +1000

    initial project setup

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './App.scss';
+
+function App() {
+  return (
+    <div className="hotel-booking">
+      <h1>Hotel Booking</h1>
+    </div>
+  );
+}
+
+export default App;
